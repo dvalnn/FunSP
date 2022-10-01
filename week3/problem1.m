3ba59beed0bee3c9924a662046cbe8b22727510a
dvalinn <tiago.andre.amorim@gmail.com>
2022-10-01 17:04:14 +0100

    frequency response code

--- week3/problem1.m
+++ week3/problem1.m
@@ -6,7 +6,13 @@ figure(1);
 stem(n, h);
 axis([-1 5 0 0.3])
 xlabel("n");
-title("Resposta impulsional");
 ylabel("h[n]");
+title("Resposta impulsional");
 
 % Resposta em frequencia
+figure(2);
+[H W] = freqz(h, 1, 512, 'whole');
+plot(W/pi, abs(H));
+xlabel('\omega/\pi \rightarrow');
+ylabel('H[\omega] \rightarrow');
+title("Resposta impulsional");
