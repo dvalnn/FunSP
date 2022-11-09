1c0920ca3b4aa66f26faf8ddb5ea3357a627fc8a
dvalinn <tiago.andre.amorim@gmail.com>
2022-11-09 10:46:38 +0000

    alterações

--- week5/p2p_problem1.m
+++ week5/p2p_problem1.m
@@ -8,14 +8,17 @@ zplane(h)
 
 % b)
 grid = [0:1/512:1 - 1/512];
-[H, W] = freqz(h, 1);
-plot(W / pi, abs(H))
-xlabel('\omega/\pi');
+% [H, W] = freqz(h, 1);
+[H, W] = freqz(h, 1, 512, 'whole', 8e3);
+plot(W, abs(H))
+xlabel('\omega');
 ylabel('Magnitude');
+printf("done\n")
+% xlabel('\omega/\pi');
 
-HFIR = sqrt(1 + 2 * alpha * cos(6 .* W) + alpha^2);
-pause
+% HFIR = sqrt(1 + 2 * alpha * cos(6 .* W) + alpha^2);
+% pause()
 
-hold on
-plot(grid, HFIR, 'm')
-hold off
+% hold on
+% % plot(grid, HFIR, 'm')
+% hold off
